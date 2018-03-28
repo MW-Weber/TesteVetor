e910d64a9318b3e728bbd97e4b1bc34f5b54a9e4
MW-Weber <Maurcio Weber@10.133.199.8>
2018-03-28 20:13:28 -0300

    Vetor Adicionado.

---
+++ src/vetor/VetorBack.java
@@ -0,0 +1,53 @@
+package vetor;
+import java.util.Random;
+
+public class VetorBack {
+        
+    /* Construtor */
+    public VetorBack(int size){
+        int Vet[];
+    }
+    
+    /* Preenche o vetor com valores aleatórios*/
+    public void randomFill(){
+        Random r= new Random();
+        
+        
+    }
+    
+    /* Implementa o método de ordenação bubbleSort*/
+    public void bubbleSort(){
+    
+    }
+    
+    /* Converte o vetor para String*/
+    public String toString(){
+    
+    }
+    /* Compara se dois vetores são iguais */
+    public boolean equals(int[] v){
+    
+    }
+    
+    /* Atribui um valor na posição pos do vetor */
+    public void setAt(int pos, int value){
+    
+    }
+    
+    /* Retorna o valor na posição pos do vetor. */
+    public int valueAt(int pos){
+    
+    }
+    
+    /* Retorna o tamanho do vetor */
+    public int length(){
+    
+    }
+    
+    /* Concatena dois vetores */
+    public Vetor Concat(Vetor v){
+    
+    }
+    
+
+}
